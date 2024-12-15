8065ce581aaa82bae201358b73d908d5f8b5a74c
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-12-15 09:32:51 -0800

    Add files via upload

---
+++ file10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    a=10;
+    b=20;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"swapping values are: "<<a<<" "<<b<<endl;
+}
